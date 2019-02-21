10c4e6af02e0a09b37326eb1a33ef1cc48e5ff93
Neil Macintyre <neil.macintyre@neilmacintyre.com>
2019-02-21 17:18:44 +0000

    connect view to model

--- Backend/langProject/captions/views.py
+++ Backend/langProject/captions/views.py
@@ -1,23 +1,28 @@
 from django.shortcuts import render
 from django.template import Context, Template
 from django.http import HttpResponse
-from .models import Caption, Word
+from .models import CaptionSegment, Word, Video
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello  World")
 
-def detail(request, question_id):
-    latest_question_list = Caption.objects.all()
-
-    output = ''.join(["<p>" + str(cap) + "</p>" for cap in latest_question_list])
-
-    return HttpResponse("You're looking at question %s." % output)
-
 def word_query(request, word, page=0):
     # query for Words
 
-    query = Word.objects.filter(word=word).values()
-    context = {"querqy_results": query[page*5:page*5+5]}
+    querys = Word.objects.filter(word=word).values()
+
+    # add title, start time, caption segment to query
+    for query in querys:
+        # get the segment containing the word
+        containing_caption_segment = CaptionSegment.objects.get(id=query['caption_segment_id_id'])
+        video_title = Video.objects.get(video_id=query['video_id_id']).title
+
+        query['start_time'] = containing_caption_segment.start_time
+        query['caption'] = containing_caption_segment.caption
+        query['title'] = video_title
+
+
+    context = {"query_results": querys[page*5:page*5+5]}
 
     return render(request, 'query_results.html', context=context)
